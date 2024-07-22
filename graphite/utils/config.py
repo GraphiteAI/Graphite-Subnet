ff745995445fcb671e798b3d0e45cd4774e8a399
GraphiteAI <team@graphite-ai.net>
2024-07-22 10:11:33 +0800

    Reduce validator concurrent forward

--- graphite/utils/config.py
+++ graphite/utils/config.py
@@ -189,7 +189,7 @@ def add_validator_args(cls, parser):
         "--neuron.num_concurrent_forwards",
         type=int,
         help="The number of concurrent forwards running at any time.",
-        default=5,
+        default=1,
     )
 
     parser.add_argument(
