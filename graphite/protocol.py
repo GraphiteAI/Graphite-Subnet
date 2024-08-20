06093aefb8005ced38149e656743e7370dfb6cf0
Team Graphite <team@graphite-ai.net>
2024-08-20 22:56:09 +0800

    hardcoding model schema to further improve speed

--- graphite/protocol.py
+++ graphite/protocol.py
@@ -23,6 +23,13 @@ import numpy as np
 import bittensor as bt
 import pprint
 import math
+import json
+import base64
+import sys
+
+rel_path = "graphite/schema.json"
+with open(rel_path, "r") as f:
+    MODEL_SCHEMA = json.load(f)
 
 class IsAlive(bt.Synapse):
     answer: Optional[str] = None
@@ -175,6 +182,83 @@ class GraphSynapse(bt.Synapse):
     '''
     problem: GraphProblem
     solution: Optional[Union[List[int], bool]] = None
+
+    def to_headers(self) -> dict:
+        """
+        Converts the state of a Synapse instance into a dictionary of HTTP headers.
+
+        This method is essential for
+        packaging Synapse data for network transmission in the Bittensor framework, ensuring that each key aspect of
+        the Synapse is represented in a format suitable for HTTP communication.
+
+        Process:
+
+        1. Basic Information: It starts by including the ``name`` and ``timeout`` of the Synapse, which are fundamental for identifying the query and managing its lifespan on the network.
+        2. Complex Objects: The method serializes the ``axon`` and ``dendrite`` objects, if present, into strings. This serialization is crucial for preserving the state and structure of these objects over the network.
+        3. Encoding: Non-optional complex objects are serialized and encoded in base64, making them safe for HTTP transport.
+        4. Size Metrics: The method calculates and adds the size of headers and the total object size, providing valuable information for network bandwidth management.
+
+        Example Usage::
+
+            synapse = Synapse(name="ExampleSynapse", timeout=30)
+            headers = synapse.to_headers()
+            # headers now contains a dictionary representing the Synapse instance
+
+        Returns:
+            dict: A dictionary containing key-value pairs representing the Synapse's properties, suitable for HTTP communication.
+        """
+        # Initializing headers with 'name' and 'timeout'
+        headers = {"name": self.name, "timeout": str(self.timeout)}
+
+        # Adding headers for 'axon' and 'dendrite' if they are not None
+        if self.axon:
+            headers.update(
+                {
+                    f"bt_header_axon_{k}": str(v)
+                    for k, v in self.axon.model_dump().items()
+                    if v is not None
+                }
+            )
+        if self.dendrite:
+            headers.update(
+                {
+                    f"bt_header_dendrite_{k}": str(v)
+                    for k, v in self.dendrite.model_dump().items()
+                    if v is not None
+                }
+            )
+
+        # Getting the fields of the instance
+        instance_fields = MODEL_SCHEMA
+
+        # Iterating over the fields of the instance
+        for field, value in instance_fields.items():
+            # If the object is not optional, serializing it, encoding it, and adding it to the headers
+            required = self.get_required_fields()
+
+            # Skipping the field if it's already in the headers or its value is None
+            if field in headers or value is None:
+                continue
+
+            elif required and field in required:
+                try:
+                    # create an empty (dummy) instance of type(value) to pass pydantic validation on the axon side
+                    serialized_value = json.dumps(value.__class__.__call__())
+                    encoded_value = base64.b64encode(serialized_value.encode()).decode(
+                        "utf-8"
+                    )
+                    headers[f"bt_header_input_obj_{field}"] = encoded_value
+                except TypeError as e:
+                    raise ValueError(
+                        f"Error serializing {field} with value {value}. Objects must be json serializable."
+                    ) from e
+
+        # Adding the size of the headers and the total size to the headers
+        headers["header_size"] = str(sys.getsizeof(headers))
+        headers["total_size"] = str(self.get_total_size())
+        headers["computed_body_hash"] = self.body_hash
+
+        return headers
     
 if __name__=="__main__":
     # run to check that each field behaves in the correct manner
