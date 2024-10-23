0b850393b1efdb9a588e47ca71850ea1e31cb7ec
Team Graphite <team@graphite-ai.net>
2024-10-23 14:50:13 +0800

    Updating init for solvers

--- graphite/solvers/__init__.py
+++ graphite/solvers/__init__.py
@@ -4,3 +4,5 @@ from .exact_solver import DPSolver
 from .greedy_solver import NearestNeighbourSolver
 from .hpn_solver import HPNSolver
 from .greedy_solver_multi import NearestNeighbourMultiSolver
+from .greedy_solver_multi_2 import NearestNeighbourMultiSolver2
+from .insertion_solver_multi import InsertionMultiSolver
