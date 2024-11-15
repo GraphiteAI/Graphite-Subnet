ef4475e5d673d5338b37e26c2b756bfdb0fc4967
GraphiteAI <team@graphite-ai.net>
2024-11-15 22:37:01 +0800

    avoid check_registered() and set_weights() causing SSL error

--- graphite/base/neuron.py
+++ graphite/base/neuron.py
@@ -130,13 +130,13 @@ class BaseNeuron(ABC):
         Wrapper for synchronizing the state of the network for the given miner or validator.
         """
         # Ensure miner or validator hotkey is still registered on the network.
-        self.check_registered()
+        # self.check_registered()
 
         if self.should_sync_metagraph():
             self.resync_metagraph()
 
-        if self.should_set_weights():
-            self.set_weights()
+        # if self.should_set_weights():
+        #     self.set_weights()
 
         # Always save state.
         self.save_state()
