7545a824a1e72e9b3488ac5d735012e92ea829f0
Team Graphite <team@graphite-ai.net>
2025-03-04 18:42:18 +0800

    adding new USA points of interest dataset

--- graphite/data/constants.py
+++ graphite/data/constants.py
@@ -2,3 +2,5 @@ ASIA_MSB_DETAILS = {"endpoint":"https://download.geofabrik.de/asia/malaysia-sing
                     "ref_id":"Asia_MSB"}
 WORLD_TSP_DETAILS = {"endpoint":"https://www.math.uwaterloo.ca/tsp/world/world.tsp.gz",
                      "ref_id":"World_TSP"}
+USA_POI_DETAILS = {"endpoint":None,
+                   "ref_id":"USA_POI"}
