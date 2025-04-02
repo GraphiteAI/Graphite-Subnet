03fb3e35fecaf0b40d479338088c82efdee95760
Sherwin-Graphite <sherwin@graphite-ai.net>
2025-04-03 02:30:29 +0900

    Update config.py
    Increased alpha

--- graphite/utils/config.py
+++ graphite/utils/config.py
@@ -210,7 +210,7 @@ def add_validator_args(cls, parser):
         "--neuron.moving_average_alpha",
         type=float,
         help="Moving average alpha parameter, how much to add of the new observation.",
-        default=0.0001,
+        default=0.0005,
     )
 
     parser.add_argument(
